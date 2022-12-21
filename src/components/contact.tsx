68d603875ce8095fb037c24dc252dcf8cdba6c28
Brian Powell <brian@powell.place>
2022-12-21 01:57:53 -0800

    Don'tcha hate naming conventions

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { FC, useMemo } from 'react'
 import Icon from '@mdi/react'
 import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api'
 
@@ -11,8 +11,11 @@ import sharedStyles from 'css/shared.module.scss'
 
 const Contact: FC<IPersonalData> = props => {
   const { isLoaded } = useLoadScript({
-    googleMapsApiKey: process.env.MAPS_API_KEY!,
+    id: 'collective-map-script',
+    googleMapsApiKey: process.env.NEXT_PUBLIC_MAPS_API_KEY!,
   })
+
+  const mapCenter = useMemo(() => ({ lat: 44, lng: -80 }), [])
 
   return (
     <article className={contactStyles.contact}>
@@ -29,9 +32,13 @@ const Contact: FC<IPersonalData> = props => {
           ) : (
             <GoogleMap
               zoom={10}
-              center={{ lat: 44, lng: -80 }}
+              center={mapCenter}
               mapContainerClassName={contactStyles.map_container}
-            ></GoogleMap>
+              // onLoad={onLoad}
+              // onUnmount={}
+            >
+              <Marker position={mapCenter} />
+            </GoogleMap>
           )}
           {/* <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d199666.5651251294!2d-121.58334177520186!3d38.56165006739519!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x809ac672b28397f9%3A0x921f6aaa74197fdb!2sSacramento%2C%20CA%2C%20USA!5e0!3m2!1sen!2sbd!4v1647608789441!5m2!1sen!2sbd"
